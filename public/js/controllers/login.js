442e1c9bbbd8ed4519fc80a78f596c47100c2fc3
Michael McDermott <michael.mcdermott@mssm.edu>
2015-03-11 15:42:12 -0400

    Login continued...

--- public/js/controllers/login.js
+++ public/js/controllers/login.js
@@ -14,7 +14,7 @@ angular.module( 'milestonesLanding.login', [
     $scope.user = {};
     $scope.login = function() {
         $http({
-            url: base + 'api/login',
+            url: base + '/login',
             method: 'POST',
             data: $scope.user
         }).then(function(result) {
