e67e9ad85b1905b44a608077e5529a2e8e9bd5ff
Michael McDermott <michael.mcdermott@mssm.edu>
2015-03-11 14:55:45 -0400

    Working with nodeJS

--- routes.js
+++ routes.js
@@ -119,7 +119,7 @@ module.exports = function(app, passport) {
         res.status(200).send('User successfully logged out');
     });
 
-    app.post(baseUrl+'/api/login', passport.authenticate('local-login'), function(req, res) {
+    app.post('/login', passport.authenticate('local-login'), function(req, res) {
         console.log(Models.User);
         token = createToken(req.user); 
         var userBlob = {
