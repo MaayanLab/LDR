72ff6f04ac094bfa27a135e7edd20a8b0449d950
Greg <ggundersen@gmail.com>
2015-05-07 13:14:05 -0400

    UNSTABLE: Debugging form tags

--- public/js/controllers/formCreate.js
+++ public/js/controllers/formCreate.js
@@ -6,301 +6,234 @@ angular.module('milestonesLanding.formCreate', [
     'ngLodash',
     'ngTagsInput'
 ])
-    // UI Router state formCreate
-    .config(function ($stateProvider) {
-        $stateProvider.state('formCreate', {
-            url: '/forms/create',
-            controller: 'FormCreateCtrl',
-            templateUrl: 'views/formCreate.html',
-            data: {
-                requiresLogin: true
+
+// UI Router state formCreate
+.config(function($stateProvider) {
+    $stateProvider.state('formCreate', {
+        url: '/forms/create',
+        controller: 'FormCreateCtrl',
+        templateUrl: 'views/formCreate.html'/*,
+        data: {
+            requiresLogin: true
+        }*/
+    });
+})
+
+.directive('enforceMaxTags', function () {
+    return {
+        require: 'ngModel',
+        link: function (scope, element, attrs, ngModelCtrl) {
+            ngModelCtrl.$parsers.push(function(value) {
+                console.log('link called');
+            });
+        }
+    };
+})
+
+.controller('FormCreateCtrl', function FormCreateController($scope, $timeout) {/*$scope, $timeout, $http, $location, $anchorScroll,
+                                                                      store, $state, $modal, lodash, FormUpdates,
+                                                                      FormPosts, DataGets, DataPosts) {*/
+
+    //$scope.user = store.get('currentUser');
+
+    //var MAX_TAGS = 100;
+
+    $scope.fields = [
+        {
+            name: 'assay',
+            title: 'Assay',
+            modalTitle: 'Assay',
+            placeholder: 'Select one assay...',
+            maxTags: 1,
+            selectedData: []
+        },
+        {
+            name: 'cellLines',
+            title: 'Cell Lines',
+            modalTitle: 'Cell Line',
+            placeholder: 'Select cell line(s)...',
+            maxTags: 100,
+            selectedData: []
+        }/*,
+        {
+            name: 'readouts',
+            title: 'Readouts',
+            modalTitle: 'Readout',
+            placeholder: 'Select readout(s)...',
+            maxTags: MAX_TAGS,
+            selectedData: []
+        },
+        {
+            name: 'perturbagens',
+            title: 'Perturbagens',
+            modalTitle: 'Perturbagen',
+            placeholder: 'Select perturbagens...',
+            maxTags: MAX_TAGS,
+            selectedData: []
+        },
+        {
+            name: 'manipulatedGene',
+            title: 'Manipulated Gene',
+            modalTitle: 'Manipulated Gene',
+            placeholder: 'Select one manipulated gene...',
+            maxTags: 1,
+            selectedData: []
+        },
+        {
+            name: 'organism',
+            title: 'Organism',
+            modalTitle: 'Organism',
+            placeholder: 'Select Organism...',
+            maxTags: 1,
+            selectedData: []
+        },
+        {
+            name: 'relevantDisease',
+            title: 'Relevant Disease',
+            modalTitle: 'Relevant Disease',
+            placeholder: 'Select Relevant Disease...',
+            maxTags: 1,
+            selectedData: []
+        },
+        {
+            name: 'experiment',
+            title: 'Experiment',
+            modalTitle: 'Experiment',
+            placeholder: 'Select Experiment',
+            maxTags: 1,
+            selectedData: []
+        },
+        {
+            name: 'analysisTools',
+            title: 'Analysis Tools',
+            modalTitle: 'Analysis Tool',
+            placeholder: 'Select Analysis Tools...',
+            maxTags: MAX_TAGS,
+            selectedData: []
+        },
+        {
+            name: 'tagsKeywords',
+            title: 'Tags/Keywords',
+            modalTitle: 'Tag/Keyword',
+            placeholder: 'Select Tag/Keywords...',
+            maxTags: MAX_TAGS,
+            selectedData: []
+        }*/
+    ];
+
+    /*function columnize(arr, size) {
+        var newArr = [];
+        for (var i=0; i<arr.length; i+=size) {
+            newArr.push(arr.slice(i, i+size));
+        }
+        return newArr;
+    }*/
+
+    //$scope.columnizedData = columnize($scope.fields, 2);
+
+    $scope.updateAutocomplete = function(val) {
+        /*return $http.get('http://146.203.54.165:7078/cell', {
+            params: {
+                name: val
+            }
+        }).then(function(response) {
+            return response.data.map(function(item) {
+                var result = {};
+                result.text = item.name;
+                result._id = new Date();
+                return result;
+            });
+        });*/
+        return $timeout(function () {
+
+            return [{
+                "text": "Tag1"
+            }, {
+                "text": "Tag2"
+            }, {
+                "text": "Tag3"
+            }, {
+                "text": "Tag4"
+            }, {
+                "text": "Tag5"
+            }, {
+                "text": "Tag6"
+            }, {
+                "text": "Tag7"
+            }, {
+                "text": "Tag8"
+            }, {
+                "text": "Tag9"
+            }, {
+                "text": "Tag10"
+            }]
+        })
+    };
+
+    // Clear form, set pristine and untouched, refresh status and date modified
+    //$scope.reset = function() {
+        //$scope.addAnother = false;
+        /*$.each($scope.fields, function(i, field) {
+            field.selectedData = [];
+        });*/
+    //};
+
+    // Clear field when user clicks 'X' button
+    /*$scope.clearSelection = function(selection) {
+        if (selection === 'Assay')
+            $scope.form.assay = {};
+        else if (selection === 'CellLine')
+            $scope.form.cellLines = [];
+        else if (selection === 'Perturbagen')
+            $scope.form.perturbagens = [];
+        else if (selection === 'Readout')
+            $scope.form.readouts = [];
+        else if (selection === 'Disease')
+            $scope.form.disease = {};
+    };
+
+    $scope.addNew = function() {
+        console.log('TODO');
+    };*/
+
+    // Post form
+    $scope.post = function() {
+
+        /*var form = {};
+        form.user = $scope.user._id;
+        form.dateModified = new Date();
+        form.center = $scope.user.center;
+
+        $.each($scope.fields, function(key, field) {
+            if (field.selectedData.length) {
+                form[field.name] = field.selectedData;
             }
         });
-    })
-    .controller('FormCreateCtrl', function FormCreateController($scope, $http, $location, $anchorScroll,
-                                                                  store, $state, $modal, lodash, FormUpdates,
-                                                                  FormPosts, DataGets, DataPosts) {
 
-        $scope.user = store.get('currentUser');
+        /*FormPosts
+            .postForm(outputForm)
+            .error(function (err) {
+                console.log(err);
+            })
+            .success(function (result) {
+                console.log('Form posted.');
+                console.log(outputForm);
+            });*/
 
-        $scope.options = [
-            {
-                name: 'assay',
-                text: '',
-                title: 'Assay',
-                modalTitle: 'Assay',
-                placeholder: 'Select Assay...',
-                multiple: false
-            },
-            {
-                name: 'cellLines',
-                title: 'Cell Lines',
-                modalTitle: 'Cell Line',
-                placeholder: 'Select Cell Lines...',
-                multiple: true
-            },
-            {
-                name: 'readouts',
-                title: 'Readouts',
-                modalTitle: 'Readout',
-                placeholder: 'Select Readouts...',
-                multiple: true
-            },
-            {
-                name: 'perturbagens',
-                title: 'Perturbagens',
-                modalTitle: 'Perturbagen',
-                placeholder: 'Select Perturbagens...',
-                multiple: true
-            },
-            {
-                name: 'manipulatedGene',
-                title: 'Manipulated Gene',
-                modalTitle: 'Manipulated Gene',
-                placeholder: 'Select Manipulated Gene...',
-                multiple: false
-            },
-            {
-                name: 'organism',
-                title: 'Organism',
-                modalTitle: 'Organism',
-                placeholder: 'Select Organism...',
-                multiple: false
-            },
-            {
-                name: 'relevantDisease',
-                title: 'Relevant Disease',
-                modalTitle: 'Relevant Disease',
-                placeholder: 'Select Relevant Disease...',
-                multiple: false
-            },
-            {
-                name: 'experiment',
-                title: 'Experiment',
-                modalTitle: 'Experiment',
-                placeholder: 'Select Experiment',
-                multiple: false
-            },
-            {
-                name: 'analysisTools',
-                title: 'Analysis Tools',
-                modalTitle: 'Analysis Tool',
-                placeholder: 'Select Analysis Tools...',
-                multiple: true
-            },
-            {
-                name: 'tagsKeywords',
-                title: 'Tags/Keywords',
-                modalTitle: 'Tag/Keyword',
-                placeholder: 'Select Tag/Keywords...',
-                multiple: true
-            }
-        ];
+        /*console.log(form);
 
-        function chunk(arr, size) {
-            var newArr = [];
-            for (var i=0; i<arr.length; i+=size) {
-                newArr.push(arr.slice(i, i+size));
-            }
-            return newArr;
-        }
+        if ($scope.addAnother) {
+            alert('Submission successful');
+            // Clear form
+            $scope.reset();
+            // Scroll to top (center)
+            $location.hash('formCenter');
+            $anchorScroll();
+        } else {
+            $state.go('forms');
+        }*/
+    };
 
-        $scope.chunkedData = chunk($scope.options, 2);
-
-
-        $scope.onSelect = function(obj, option) {
-            $scope.form[option] = obj;
-        };
-
-        var tagCache = {};
-
-        $scope.onSelectCache = function(key, option) {
-            $scope.form[option].push( tagCache[key.text] );
-        };
-
-        $scope.onRemoveCache = function(key, option) {
-            lodash.remove($scope.form[option], function(obj) {
-                return obj.formatted_address === key.text;
-            });
-        };
-
-        $scope.funcAsync = function(val, multipleBool) {
-            if (val === '') {
-                return;
-            }
-            return $http.get('http://maps.googleapis.com/maps/api/geocode/json', {
-                params: {
-                    address: val,
-                    sensor: false
-                }
-            }).then(function(response) {
-                return response.data.results.map(function(item){
-                    tagCache[item.formatted_address] = item;
-                    return (multipleBool ? item.formatted_address : item);
-                });
-            });
-        };
-
-        // Date picker
-        $scope.clear = function () {
-            $scope.form.releaseDates.levelOne = null;
-            $scope.form.releaseDates.levelTwo = null;
-            $scope.form.releaseDates.levelThree = null;
-            $scope.form.releaseDates.levelFour = null;
-
-        };
-
-        // Disable weekend selection
-        $scope.disabledDate = function (date, mode) {
-            return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
-        };
-
-        $scope.minDate = new Date();
-
-        $scope.lvlOneOpen = function ($event) {
-            $event.preventDefault();
-            $event.stopPropagation();
-
-            $scope.lvlOneOpened = true;
-        };
-
-        $scope.lvlTwoOpen = function ($event) {
-            $event.preventDefault();
-            $event.stopPropagation();
-
-            $scope.lvlTwoOpened = true;
-        };
-
-        $scope.lvlThreeOpen = function ($event) {
-            $event.preventDefault();
-            $event.stopPropagation();
-
-            $scope.lvlThreeOpened = true;
-        };
-
-        $scope.lvlFourOpen = function ($event) {
-            $event.preventDefault();
-            $event.stopPropagation();
-
-            $scope.lvlFourOpened = true;
-        };
-
-        $scope.dateOptions = {
-            formatYear: 'yy',
-            startingDay: 1
-        };
-        $scope.format = 'MM/dd/yyyy';
-        // End date picker
-
-        // Clear form, set pristine and untouched, refresh status and date modified
-        $scope.reset = function (inpForm) {
-            if (inpForm) {
-                inpForm.$setPristine();
-                inpForm.$setUntouched();
-            }
-
-            $scope.addAnother = false;
-            $scope.showValidation = false;
-
-            $scope.form = {};
-            $scope.form.user = $scope.user._id;
-            $scope.form.status = 'awaiting approval';
-            $scope.form.dateModified = new Date();
-            $scope.form.center = $scope.user.center;
-            $.each($scope.options, function(i, option) {
-                $scope.form[option.name] = option.multiple ? [] : {};
-            });
-        };
-
-        // Clear field when user clicks 'X' button
-        $scope.clearSelection = function(selection) {
-            if (selection === 'Assay')
-                $scope.form.assay = {};
-            else if (selection === 'CellLine')
-                $scope.form.cellLines = [];
-            else if (selection === 'Perturbagen')
-                $scope.form.perturbagens = [];
-            else if (selection === 'Readout')
-                $scope.form.readouts = [];
-            else if (selection === 'Disease')
-                $scope.form.disease = {};
-        };
-
-        $scope.addNew = function() {
-            console.log('TODO');
-        };
-
-        // Update form
-        $scope.updateForm = function () {
-            $scope.form.status = 'awaiting approval';
-            $scope.form.dateModified = new Date();
-            FormUpdates.updateForm($scope.form).success(function (err, result) {
-                if (err)
-                    console.log(err);
-            });
-        };
-
-        // Post form
-        $scope.post = function () {
-
-            console.log($scope.form);
-
-            $scope.showValidation = true;
-            var outputForm = {};
-            $.each($scope.form, function (key, value) {
-                if (value)
-                    outputForm[key] = value;
-            });
-
-            // Get counts of meta-data
-            outputForm.cellLineCount = $scope.form.cellLines.length;
-            outputForm.perturbagenCount = $scope.form.perturbagens.length;
-            outputForm.readoutCount = $scope.form.readouts.length;
-
-            // TODO: Only push IDs of model systems, this allows for them to change in the form when changed elsewhere
-            /*outputForm.assay = $scope.form.assay._id;
-            outputForm.cellLines = [];
-            for (var i = 0; i < outputForm.cellLineCount; i++) {
-                outputForm.cellLines.push($scope.form.cellLines[i]._id);
-            }
-            outputForm.perturbagens = [];
-            for (var j = 0; j < outputForm.perturbagenCount; j++) {
-                outputForm.perturbagens.push($scope.form.perturbagens[j]._id);
-            }
-            outputForm.readouts = [];
-            for (var k = 0; k < outputForm.readoutCount; k++) {
-                outputForm.readouts.push($scope.form.readouts[k]._id);
-            }
-            */
-
-            console.log(outputForm);
-
-            FormPosts
-                .postForm(outputForm)
-                .error(function (err) {
-                    console.log(err);
-                })
-                .success(function (result) {
-                    console.log('Form posted.');
-                    console.log(outputForm);
-                });
-
-            if ($scope.addAnother) {
-                alert('Post successful');
-
-                // Clear form
-                $scope.reset();
-
-                // Scroll to top (center)
-                $location.hash('formCenter');
-                $anchorScroll();
-            }
-
-            else {
-                $state.go('forms');
-            }
-        };
-
-        // Init form. Will be changed to inside a conditional when editing is implemented
-        $scope.reset();
-    });
+    // Init form. Will be changed to inside a conditional when editing is implemented
+    //$scope.reset();
+});
