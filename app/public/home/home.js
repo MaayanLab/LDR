dd8005f52a9a94113376c228795cd70e24c1aa0c
Michael McDermott <michael@mgmcdermott.com>
2015-07-22 14:36:53 -0400

    Removed dependency on infinite scroll

--- app/public/home/home.js
+++ app/public/home/home.js
@@ -4,7 +4,6 @@ angular.module('ldr.home', [
     'ldr.api',
     'ui.router',
     'angular-storage',
-    'infinite-scroll',
     'ngLodash',
     'wu.masonry'
 ])
@@ -17,40 +16,7 @@ angular.module('ldr.home', [
         });
     })
 
-    /*.factory('ReleasesLoader', function(api) {
-        var ReleasesLoader = function() {
-            this.items = [];
-            this.busy = false;
-            this.after = '';
-        };
-
-        ReleasesLoader.prototype.nextPage = function() {
-            if (this.busy || this.empty) {
-                return;
-            }
-            this.busy = true;
-
-            api('releases/approved/' + this.after)
-                .get()
-                .success(function(releases) {
-                    if (!releases.length) {
-                        this.empty = true;
-                        return;
-                    }
-                    this.empty = false;
-
-                    for (var i = 0; i < releases.length; i++) {
-                        this.items.push(releases[i]);
-                    }
-                    this.after = this.items[this.items.length - 1]._id;
-                    this.busy = false;
-                }.bind(this)
-            );
-        };
-        return ReleasesLoader;
-    })*/
-
-    .controller('HomeController', function($scope, /*ReleasesLoader,*/ api) {
+    .controller('HomeController', function($scope, api) {
 
         $scope.approvedReleases = [];
         $scope.query = '';
